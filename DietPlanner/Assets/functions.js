929cbddadca5cff15465beff833f001bb2e4f0fd
Fernando <fernandoairescastello@gmail.com>
2017-09-19 23:39:20 -0300

    Initial commit

---
+++ DietPlanner/Assets/functions.js
@@ -0,0 +1,38 @@
+﻿
+function CommonInit()
+{
+    $('#content').fadeIn('slow');
+    $('#tdResults').animate({ 'padding-top': '-50px' }, 1000);
+}
+
+function MainMenu()
+{
+    window.location.href = 'index.htm';
+}
+
+function CalorieCalculator()
+{
+    window.location.href = 'calorie_calc_angularjs.htm';
+}
+
+function FoodListing()
+{
+}
+
+function DietProgression()
+{
+}
+
+function DailyMenu()
+{
+}
+
+function SaveData(key, value)
+{
+    localStorage.setItem(key, value);
+}
+
+function LoadData(key)
+{
+    return localStorage.getItem(key);
+}
---
+++ DietPlanner/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+using Android.Views;
+
+namespace DietPlanner
+{
+    [Activity(Label = "DietPlanner", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private WebView Wv;
+        private const string InitialPage = "file:///android_asset/index.htm";
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            Window.AddFlags(WindowManagerFlags.Fullscreen);
+            SetContentView(Resource.Layout.Main);
+
+            Wv = FindViewById<WebView>(Resource.Id.WebView);
+            Wv.Settings.JavaScriptEnabled = true;
+            Wv.Settings.DomStorageEnabled = true;
+            Wv.Settings.DatabaseEnabled = true;
+            Wv.Settings.DatabasePath = @"/data/data/" + Wv.Context.PackageName + "/databases/";
+
+            Wv.LoadUrl(InitialPage);
+        }
+
+        public override void OnBackPressed()
+        {
+            Wv.LoadUrl(InitialPage);
+        }
+    }
+}
